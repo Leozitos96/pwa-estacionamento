6ae57051511cbf39431ce208acd6d2d52095a71f
Leozitos96 <marginaaladoleo@gmail.com>
2025-10-07 20:49:29 -0300

    codigo fonte

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB52N1JBgWdWoxiZfobH0NxcRaNh6N3-Y8",
+  authDomain: "estacionamento-pwa12.firebaseapp.com",
+  projectId: "estacionamento-pwa12",
+  storageBucket: "estacionamento-pwa12.firebasestorage.app",
+  messagingSenderId: "1036357381383",
+  appId: "1:1036357381383:web:3aebfc0703e6adb1cb29a2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
